a7e813c32714529fcd8fcded5eba43a724968681
João Moraski <joaomoraskilunkes@gmail.com>
2023-12-08 01:48:37 -0300

    colocando a musiquinah de volta

--- souls-of-the-hollow/game_init.gd
+++ souls-of-the-hollow/game_init.gd
@@ -3,6 +3,6 @@ extends Node2D
 
 func _ready() -> void:
 	game_controller.init($Player, $Player/Camera2D, $PrincipalUI/ProgressBar)
-#	$Audio.play(4.6)
-#	$Audio.stream.loop = true
+	$Audio.play(4.6)
+	$Audio.stream.loop = true
 	
